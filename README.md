48288f51bb6ead0438f55e93b2e4c7401444a7fc
darwin808 <darwin.apolinario12@gmail.com>
2022-05-27 02:37:09 +0800

    added new accounts endpoints and crud functions for marketplace

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 # Zesty.io API Fetch Wrapper
 
-A javascript fetch wrapper for the Zesty.io APIs for frontend clients. For a backend node wrapper, use the [Zesty.io Node SDK](https://www.npmjs.com/package/@zesty-io/sdk) 
+A javascript fetch wrapper for the Zesty.io APIs for frontend clients. For a backend node wrapper, use the [Zesty.io Node SDK](https://www.npmjs.com/package/@zesty-io/sdk)
 
 ### API Service the Wrapper can Access
 
-- Instances API https://instances-api.zesty.org/
-- Accounts API https://accounts-api.zesty.org/
-- Auth API https://auth-api.zesty.org/
-- Media API https://media-api.zesty.org/
+-  Instances API https://instances-api.zesty.org/
+-  Accounts API https://accounts-api.zesty.org/
+-  Auth API https://auth-api.zesty.org/
+-  Media API https://media-api.zesty.org/
 
 Note all endpoints are ported. If you wish to port any missing endpoints, create a fork and pull request.
 
@@ -33,4 +33,22 @@ ZestyAPI = new Zesty.FetchWrapper(instanceZUID,userAppSID)
 
 console.log(ZestyAPI.getModels())
 
+```
+
+# How to run in Development
+
+```
+npm start
+```
+
+then add this in the head of Html
+
+```
+  <script src="http://localhost:8080/dist/index.js" />
+```
+
+# How to build in Production
+
+```
+git checkout main && npm run build && npm run release
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,44 @@
-const path = require("path");
+const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-    library: "Zesty",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
-  mode: "development",
-};
+   entry: path.resolve(__dirname, "src/index.js"),
+   devtool: false,
+   output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "index.js",
+      library: "Zesty",
+      libraryTarget: "umd",
+   },
+   performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+   },
+   optimization: {
+      minimize: true,
+      minimizer: [
+         new TerserPlugin({
+            parallel: true,
+            minify: TerserPlugin.swcMinify,
+            terserOptions: {
+               format: {
+                  comments: false,
+               },
+            },
+            extractComments: false,
+         }),
+      ],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: "babel-loader",
+         },
+      ],
+   },
+   plugins: [new CleanWebpackPlugin()],
+}
