45fd2e73b0475e49517dba36931bda8092eee3d2
darwin808 <darwin.apolinario12@gmail.com>
2022-07-21 11:23:26 +0800

    test(test): test

--- src/FetchWrapper.test.ts
+++ src/FetchWrapper.test.ts
@@ -6,33 +6,29 @@ const userAppSID = process.env.userAppSID
 const instanceZUID = process.env.instanceZUID
 
 describe("Fetchwrapper functions testing ", () => {
-   console.log(userAppSID, instanceZUID, "#######################################")
+   console.log(process.env, "#######################################")
+   const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
    it("verify --- expect 200 code", async () => {
-      const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
       const res = await ZestyAPI.verify()
       expect(res.code).toBe(200)
    })
    it("getModel --- expect data to be present ", async () => {
-      const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
       const res = await ZestyAPI.getModels()
       expect(res.error).toBeFalsy()
       expect(res.data).toBeTruthy()
    })
    it("searchItems --- expect data to be present ", async () => {
-      const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
       const res = await ZestyAPI.searchItems()
       expect(res.data).toBeTruthy()
       expect(res.error).toBeFalsy()
    })
    it("getViews --- expect data to be present ", async () => {
-      const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
       const res = await ZestyAPI.getViews()
       expect(res.data).toBeTruthy()
       expect(res.error).toBeFalsy()
    })
 
    it("getView --- expect data to be present ", async () => {
-      const ZestyAPI = new FetchWrapper(instanceZUID, userAppSID)
       const res = await ZestyAPI.getView(instanceZUID)
       expect(res.data).toBeTruthy()
       expect(res.error).toBeFalsy()
