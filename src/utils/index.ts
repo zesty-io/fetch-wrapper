880622b01effa8fe9e1e809baec5722acbd2d180
darwin.apolinario <darwin.apolinario@gmail.com>
2024-06-13 17:26:52 +0800

    test: imrpove unit testing

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,3 +25,27 @@ export const date7DaysAgo = new Date(new Date().setDate(new Date().getDate() - 7
    .slice(0, 10)
 
 export const dateToday = new Date().toISOString().slice(0, 10)
+
+export const authUser = async (): Promise<string> => {
+   const myHeaders = new Headers()
+
+   const formdata = new FormData()
+   formdata.append("email", process.env.TEST_EMAIL)
+   formdata.append("password", process.env.TEST_PASSWORD)
+
+   const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: formdata,
+      redirect: "follow",
+   }
+   try {
+      // @ts-ignore
+      const response = await fetch("https://auth.api.zesty.io/login", requestOptions)
+      const result = await response.json()
+      return result.data.data
+   } catch (error) {
+      console.log("error", error)
+      return ""
+   }
+}
